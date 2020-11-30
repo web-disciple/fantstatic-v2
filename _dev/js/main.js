a324875eb199db19f22656e34fb969ae2f747d52
Matton <45633887+monsieurwai@users.noreply.github.com>
2020-11-30 10:53:57 +0100

    init

---
+++ _dev/js/main.js
@@ -0,0 +1,5 @@
+import HelloWorld from "../../../fantstatic-v2-components/hello-world/V-0/hello_world";
+
+const helloWorld = new HelloWorld();
+
+helloWorld.init();
---
+++ dist/js/main.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  class HelloWorld {
+    constructor() {}
+    init() {
+      console.log("hello world");
+    }
+  }
+
+  const helloWorld = new HelloWorld();
+
+  helloWorld.init();
+
+}());
+//# sourceMappingURL=main.js.map
